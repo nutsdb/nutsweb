b805c0d92c37afcf42b5b37d6dd7314edc17b176
xgs <1150840779@qq.com>
2022-09-06 14:17:14 +0800

    Change the title and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './pages/Login/login';
 import Index from './pages/Index/index';
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    document.title = "NutsWeb";
+  }, [])
   return (
     <Router>
       <Routes>
