dc169676b5e07e4675856bca246f68e5690a9abe
Gaosong <shimaring@qq.com>
2022-11-21 23:12:21 +0800

    fix invalid url error

--- src/backend/GetStringData.ts
+++ src/backend/GetStringData.ts
@@ -5,8 +5,15 @@ let token = localStorage.getItem('token');
 let ip = localStorage.getItem('ip');
 let port = localStorage.getItem('port');
 
+function loadData() {
+  token = localStorage.getItem('token');
+  ip = localStorage.getItem('ip');
+  port = localStorage.getItem('port');
+}
+
 // Get all KVS
 export async function GetAllString(bucket: string): Promise<any> {
+  loadData();
   const Url = `http://${ip}:${port}`;
   return await axios.get(
     `${Url}/string/scan/${bucket}/getAll?token=${token}`,
@@ -15,6 +22,7 @@ export async function GetAllString(bucket: string): Promise<any> {
 
 // Get a specific kV
 export async function GetValueString(bucket: string, key: string): Promise<any> {
+  loadData();
   const Url = `http://${ip}:${port}`;
   return await axios.get(
     `${Url}/string/get/${bucket}/${key}?token=${token}`,
@@ -23,6 +31,7 @@ export async function GetValueString(bucket: string, key: string): Promise<any> 
 
 // Get a range kV
 export async function GetRangeValueString(bucket: string, start: string, end: string): Promise<any> {
+  loadData();
   const Url = `http://${ip}:${port}`;
   return await axios.get(
     `${Url}/string/scan/${bucket}/rangeScan?start=${start}&end=${end}&token=${token}`,
